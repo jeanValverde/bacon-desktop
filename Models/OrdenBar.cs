0c2bd200ac13e1e87c53a87025959c516c172896
Felipe González <felipegonzalezvicencio@gmail.com>
2019-10-16 12:28:57 -0300

    Cambios escritorio

---
+++ Models/OrdenBar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bacon_desktop.Models
+{
+    public class OrdenBar
+    {
+        private Orden orden;
+        private List<RecetaOrdenada> recetaOrdenada;
+
+        public OrdenBar()
+        {
+
+        }
+
+        public OrdenBar(Orden orden, List<RecetaOrdenada> recetaOrdenada)
+        {
+            this.orden = orden;
+            this.recetaOrdenada = recetaOrdenada;
+        }
+
+        public Orden Orden { get => orden; set => orden = value; }
+        public List<RecetaOrdenada> RecetaOrdenada { get => recetaOrdenada; set => recetaOrdenada = value; }
+    }
+}
